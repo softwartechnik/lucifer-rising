10d7fd4befea70d0eab037b17be773c75577212b
LenaW <moccaleni@googlemail.com>
2020-01-18 11:52:38 +0100

    add equals fix parameter name change choice from set to array add testcases

--- lucifer-rising-chat/src/main/java/gson/NodeModel.java
+++ lucifer-rising-chat/src/main/java/gson/NodeModel.java
@@ -1,59 +1,98 @@
 package gson;
 
-import de.softwartechnik.lucifer.tree.QuestionNode;
-
-import java.util.Set;
-import java.util.function.Predicate;
+import java.util.Arrays;
 
 public interface NodeModel {
 }
 
 final class MessageNodeModel implements NodeModel {
-  String id;
-  String message;
-  String nextNode;
+  public String id;
+  public String message;
+  public String nextNode;
 
   public MessageNodeModel(String id, String message, String nextNode) {
     this.id = id;
     this.message = message;
     this.nextNode = nextNode;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof MessageNodeModel)) {
+      return false;
+    }
+    MessageNodeModel m = (MessageNodeModel)o;
+    return m.id.equals(id) && m.message.equals(message) && m.nextNode.equals(nextNode);
+  }
 }
 
 final class QuestionNodeModel implements NodeModel {
-  String id;
-  String question;
-  Set<Choice> choices;
+  public String id;
+  public String question;
+  public Choice[] choices;
 
-  public QuestionNodeModel(String id, String question, Set<Choice> choices) {
+  public QuestionNodeModel(String id, String question, Choice[] choices) {
     this.id = id;
     this.question = question;
     this.choices = choices;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof QuestionNodeModel)) {
+      return false;
+    }
+    QuestionNodeModel q = (QuestionNodeModel) o;
+    return q.id.equals(id) && q.question.equals(question) && Arrays.equals(q.choices, choices);
+  }
 }
 
 final class DeathNodeModel implements NodeModel {
-  String id;
+  public String id;
 
   public DeathNodeModel(String id) {
     this.id = id;
   }
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof DeathNodeModel)) {
+      return false;
+    }
+    DeathNodeModel d = (DeathNodeModel) o;
+    return d.id.equals(id);
+  }
 }
 
 final class EndNodeModel implements NodeModel {
-  String id;
+  public String id;
 
   public EndNodeModel(String id) {
     this.id = id;
   }
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof EndNodeModel)) {
+      return false;
+    }
+    EndNodeModel e = (EndNodeModel) o;
+    return e.id.equals(id);
+  }
 }
 
 final class Choice {
-  private final String matcher;
-  private final String nextNode;
+  public final String pattern;
+  public final String nextNode;
 
   public Choice(String matcher, String nextNode) {
-    this.matcher = matcher;
+    this.pattern = matcher;
     this.nextNode = nextNode;
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Choice)) {
+      return false;
+    }
+    Choice c = (Choice) o;
+    return c.pattern.equals(pattern) && c.nextNode.equals(nextNode);
   }
 }
--- lucifer-rising-chat/src/test/java/gson/GsonFactoryTest.java
+++ lucifer-rising-chat/src/test/java/gson/GsonFactoryTest.java
@@ -1,8 +1,10 @@
 package gson;
 
+import de.softwartechnik.lucifer.tree.QuestionNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.spec.ChaCha20ParameterSpec;
 import java.util.List;
 import java.util.Set;
 
@@ -23,9 +25,36 @@ public class GsonFactoryTest {
   void testGetQuestionNodeFromFile() {
     String file = "test_question_node.json";
     QuestionNodeModel expectedNode = new QuestionNodeModel("2",
-      "How much is the fish?", Set.of(new Choice("RegEx", "3"), new Choice("OtherRegEx", "4")));
+      "How much is the fish?", new Choice[]{new Choice("RegEx", "3"), new Choice("OtherRegEx", "4")});
     List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
 
-    assertEquals(expectedNode.getClass(), result.get(0).getClass());
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetMessageNodeFromFile() {
+    String file = "test_message_node.json";
+    MessageNodeModel expectedNode = new MessageNodeModel("2", "This much is the fish", "3");
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetDeathNodeFromFile() {
+    String file = "test_death_node.json";
+    DeathNodeModel expectedNode = new DeathNodeModel("2");
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetEndNodeFromFile() {
+    String file = "test_end_node.json";
+    EndNodeModel expectedNode = new EndNodeModel("2");
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
   }
 }
