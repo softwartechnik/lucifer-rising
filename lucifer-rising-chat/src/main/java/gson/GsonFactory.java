8808365b65223f7b81e049a539977f5332b9edc3
Moccaleni <50426447+Moccaleni@users.noreply.github.com>
2020-02-01 11:08:53 +0100

    Chat/gson (#50)
    * json to pojo with gson - getting started
    
    * add HashMap attribute
    
    * experiment with tests and test file
    
    * add nodes
    modify nodes
    
    * add TODO for fixing GSON
    add example node for each type
    delete old test model
    
    * adapt gson to new model
    deleted old useless Test
    add new test file
    
    * add RuntimeTypeAdapterFactory
    adapt gson to to RuntimeTypeAdapterFactory
    delete unused model classes
    
    * add NodeModel
    refactor Model classes
    fix GsonFactory
    modify test file
    
    * add message attribute
    
    * add end node
    add details to node messages
    
    * add regular expressions to question node choices
    
    * Added nodes_outbreak.json
    
    * clean up
    add test files
    refactor test case
    
    * fix assertion
    
    * add equals
    fix parameter name
    change choice from set to array
    add testcases
    
    * add test method
    
    * add delay attribute
    add test case for invalid types
    
    * Add Node delays
    
    * Fixed issues in nodes_outbreak.json
    
    Co-authored-by: Felix Klauke <info@felix-klauke.de>

---
+++ lucifer-rising-chat/src/main/java/gson/GsonFactory.java
@@ -0,0 +1,30 @@
+package gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class GsonFactory {
+
+  public List<NodeModel> getNodesFromFile(String myFilePath) {
+
+    FileReader reader = null;
+    try {
+      reader = new FileReader(System.getProperty("user.dir") + myFilePath);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    RuntimeTypeAdapterFactory<NodeModel> adapter = RuntimeTypeAdapterFactory.of(NodeModel.class, "type")
+      .registerSubtype(MessageNodeModel.class, "message")
+      .registerSubtype(QuestionNodeModel.class, "question")
+      .registerSubtype(DeathNodeModel.class, "death")
+      .registerSubtype(EndNodeModel.class, "end");
+
+    Gson gson = new GsonBuilder().registerTypeAdapterFactory(adapter).create();
+    return Arrays.asList(gson.fromJson(reader, NodeModel[].class));
+  }
+}
---
+++ lucifer-rising-chat/src/test/java/gson/GsonFactoryTest.java
@@ -0,0 +1,79 @@
+package gson;
+
+import com.google.gson.JsonParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class GsonFactoryTest {
+
+  private final String filepathNodeDir = "/src/test/resources/nodefiles/";
+  private GsonFactory gsonFactory;
+
+  @BeforeEach
+  void setUp() {
+    gsonFactory = new GsonFactory();
+  }
+
+  @Test
+  void testGetQuestionNodeFromFile() {
+    String file = "test_question_node.json";
+    QuestionNodeModel expectedNode = new QuestionNodeModel("2",
+      "How much is the fish?", new Choice[]{new Choice("RegEx", "3"), new Choice("OtherRegEx", "4")}, 1000);
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetMessageNodeFromFile() {
+    String file = "test_message_node.json";
+    MessageNodeModel expectedNode = new MessageNodeModel("2", "This much is the fish", "3",1000);
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetDeathNodeFromFile() {
+    String file = "test_death_node.json";
+    DeathNodeModel expectedNode = new DeathNodeModel("2",1000);
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testGetEndNodeFromFile() {
+    String file = "test_end_node.json";
+    EndNodeModel expectedNode = new EndNodeModel("2",1000);
+    List<NodeModel> result = gsonFactory.getNodesFromFile(filepathNodeDir + file);
+
+    assertEquals(expectedNode, result.get(0));
+  }
+
+  @Test
+  void testWithNonExistingFile() {
+    String file = "xxxx.json";
+    Throwable exception = assertThrows(NullPointerException.class,
+      () -> {
+        gsonFactory.getNodesFromFile(filepathNodeDir + file);
+        ;
+      });
+  }
+
+  @Test
+  void testFileWithInvalidNode() {
+    String file = "test_invalid_node.json";
+    Throwable exception = assertThrows(JsonParseException.class,
+      () -> {
+        gsonFactory.getNodesFromFile(filepathNodeDir + file);
+        ;
+      });
+  }
+}
