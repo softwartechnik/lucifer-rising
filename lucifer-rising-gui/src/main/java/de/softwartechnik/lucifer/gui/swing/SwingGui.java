e66f856e0177ae56c91a3371d0c33e7024056fe2
LenaW <moccaleni@googlemail.com>
2020-02-03 18:04:47 +0100

    add main add logout button to menu

--- lucifer-rising-gui/src/main/java/de/softwartechnik/lucifer/gui/swing/SwingGui.java
+++ lucifer-rising-gui/src/main/java/de/softwartechnik/lucifer/gui/swing/SwingGui.java
@@ -1,6 +1,7 @@
 package de.softwartechnik.lucifer.gui.swing;
 
 import de.softwartechnik.lucifer.gui.swing.controller.Controller;
+import de.softwartechnik.lucifer.gui.swing.view.*;
 
 import javax.inject.Inject;
 
@@ -16,5 +17,18 @@ public final class SwingGui {
   public void initialize() {
     controller.setUpListener();
     controller.showLogin();
+  }
+
+  public static void main(String[] args) {
+    //TODO: fix this with Dependency injection
+    SwingGui gui = new SwingGui(new Controller(
+      new MainFrame(),
+      new LoginView(new LoginPanel()),
+      new MenuView(new MenuPanel()),
+      new ScenarioView(new ScenarioPanel()),
+      new SignUpView(new SignUpPanel())
+    ));
+
+    gui.initialize();
   }
 }
