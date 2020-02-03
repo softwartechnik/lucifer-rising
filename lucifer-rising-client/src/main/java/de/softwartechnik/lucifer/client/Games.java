94d951653bfa3cd1d0c53e28744db7cd3aa09e73
Felix Klauke <info@felix-klauke.de>
2020-02-03 08:49:35 +0100

    Add method to find user by id

---
+++ lucifer-rising-client/src/main/java/de/softwartechnik/lucifer/client/Games.java
@@ -0,0 +1,4 @@
+package de.softwartechnik.lucifer.client;
+
+public final class Games {
+}
--- lucifer-rising-session/src/main/java/de/softwartechnik/lucifer/session/user/Users.java
+++ lucifer-rising-session/src/main/java/de/softwartechnik/lucifer/session/user/Users.java
@@ -3,15 +3,29 @@ package de.softwartechnik.lucifer.session.user;
 import de.softwartechnik.lucifer.user.User;
 import de.softwartechnik.lucifer.user.UserService;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/user")
 public class Users {
   @Inject
   private UserService userService;
+
+  @GET
+  public Response find(
+    @QueryParam("userId") int userId
+  ) {
+    User user = userService.read(userId);
+    if (user == null) {
+      return Response.status(Status.NOT_FOUND).build();
+    }
+    return Response.ok(user).build();
+  }
+
 
   @POST
   public Response register(
