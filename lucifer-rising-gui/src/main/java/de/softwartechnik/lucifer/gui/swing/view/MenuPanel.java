32bcad4282a9c5127161eae00e83c17bb52a8cc7
hdainester <4290103-hdainester1@users.noreply.gitlab.com>
2020-02-04 18:21:21 +0100

    Update MenuPanel bit more interesting look

--- lucifer-rising-gui/src/main/java/de/softwartechnik/lucifer/gui/swing/view/MenuPanel.java
+++ lucifer-rising-gui/src/main/java/de/softwartechnik/lucifer/gui/swing/view/MenuPanel.java
@@ -1,6 +1,8 @@
 package de.softwartechnik.lucifer.gui.swing.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -43,19 +45,24 @@ public final class MenuPanel extends BackgroundPanel {
     logoutButton = new JButton("Logout");
     constraints.gridx = 0;
     constraints.gridy = 3;
-    constraints.gridwidth = 2;
+    constraints.gridwidth = 1;
     add(logoutButton, constraints);
   }
 
   private JPanel buildScenarioPanel() {
     JPanel innerScenarioPanel = new JPanel(new BorderLayout());
+    innerScenarioPanel.setOpaque(false);
 
     JPanel scenarioHeadPanel = new JPanel();
+    scenarioHeadPanel.setBackground(new Color(255, 255, 255, 100));
+
     JLabel scenarioLabel = new JLabel("Szenarien");
+    scenarioLabel.setFont(scenarioLabel.getFont().deriveFont(Font.BOLD, 22));
     scenarioHeadPanel.add(scenarioLabel);
     innerScenarioPanel.add(scenarioHeadPanel, BorderLayout.NORTH);
 
     JPanel bodyScenarioPanel = new JPanel(new GridLayout(1,2));
+    bodyScenarioPanel.setOpaque(false);
     bodyScenarioPanel.add(buildZombieScenarioPanel());
     bodyScenarioPanel.add(buildApokalypseScenarioPanel());
     innerScenarioPanel.add(bodyScenarioPanel, BorderLayout.CENTER);
@@ -64,38 +71,52 @@ public final class MenuPanel extends BackgroundPanel {
   }
 
   private JPanel buildZombieScenarioPanel() {
-    JPanel zombieScenarioPanel = new JPanel(new BorderLayout());
+    JPanel zombieScenarioPanel = new BackgroundPanel("/zombie_hands.png");
+    zombieScenarioPanel.setBackground(new Color(255, 255, 255, 100));
+
     JLabel zombieScenarioHeadLabel = new JLabel("Zombie Outbreak");
     JPanel zombieScenarioHeadPanel = new JPanel();
+    zombieScenarioHeadLabel.setFont(zombieScenarioHeadLabel.getFont().deriveFont(Font.BOLD));
+    zombieScenarioHeadLabel.setForeground(Color.WHITE);
+    zombieScenarioHeadPanel.setBackground(new Color(0, 0, 0, 150));
     zombieScenarioHeadPanel.add(zombieScenarioHeadLabel);
     zombieScenarioPanel.add(zombieScenarioHeadPanel, BorderLayout.NORTH);
     JLabel zombieScenarioInfoPanel = new JLabel(
-      "<html><p style=\"width=400px\">Ein gew\u00f6hnlicher Tag an der FH oder doch "
+      "<html><p style=\"width=500px\">Ein gew\u00f6hnlicher Tag an der FH oder doch "
         + "ein ausgewachsenes Abenteuer mit heiklen Situationen, schwierigen"
         + " Entscheidungen und Nervenkitzel pur? Hier ist survival instinct gefragt.</p></html>"
     );
+    zombieScenarioInfoPanel.setForeground(Color.WHITE);
     zombieScenarioPanel.add(zombieScenarioInfoPanel, BorderLayout.CENTER);
     zombieScenarioButton = new JButton("Zombie Outbreak spielen");
     JPanel zombieScenarioButtonPanel = new JPanel();
+    zombieScenarioButtonPanel.setBackground(zombieScenarioHeadPanel.getBackground());
     zombieScenarioButtonPanel.add(zombieScenarioButton);
     zombieScenarioPanel.add(zombieScenarioButtonPanel, BorderLayout.SOUTH);
     return zombieScenarioPanel;
   }
 
   private JPanel buildApokalypseScenarioPanel() {
-    JPanel apokalypseScenarioPanel = new JPanel(new BorderLayout());
+    JPanel apokalypseScenarioPanel = new BackgroundPanel("/meteor_rain.png");
+    apokalypseScenarioPanel.setBackground(new Color(255, 255, 255, 100));
+
     JLabel apokalypseScenarioHeadLabel = new JLabel("Apokalypse");
     JPanel apokalypseScenarioHeadPanel = new JPanel();
+    apokalypseScenarioHeadLabel.setFont(apokalypseScenarioHeadLabel.getFont().deriveFont(Font.BOLD));
+    apokalypseScenarioHeadLabel.setForeground(Color.WHITE);
+    apokalypseScenarioHeadPanel.setBackground(new Color(0, 0, 0, 150));
     apokalypseScenarioHeadPanel.add(apokalypseScenarioHeadLabel);
     apokalypseScenarioPanel.add(apokalypseScenarioHeadPanel, BorderLayout.NORTH);
     JLabel apokalypseScenarioInfoPanel = new JLabel(
-      "<html><p style=\"width=400px\">Wem nach d\u00fcsterer (Spiel-)Stimmung ist, der"
+      "<html><p style=\"width=500px\">Wem nach d\u00fcsterer (Spiel-)Stimmung ist, der"
         + " ist hier genau richtig. Der Mann auf dem wei\u00dfen Pferd l\u00e4utet die Apokalypse"
         + " ein! Wird die Welt in den Abgrund sinken?</p></html>"
     );
+    apokalypseScenarioInfoPanel.setForeground(Color.WHITE);
     apokalypseScenarioPanel.add(apokalypseScenarioInfoPanel, BorderLayout.CENTER);
     apokalypseScenarioButton = new JButton("Apokalypse spielen");
     JPanel apokalypseScenarioButtonPanel = new JPanel();
+    apokalypseScenarioButtonPanel.setBackground(apokalypseScenarioHeadPanel.getBackground());
     apokalypseScenarioButtonPanel.add(apokalypseScenarioButton);
     apokalypseScenarioPanel.add(apokalypseScenarioButtonPanel, BorderLayout.SOUTH);
     return apokalypseScenarioPanel;
